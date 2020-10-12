b482258e5e082288ec4a8737e55be668d420ed14
yves-marieRaiffaud <yves-marie.raiffaud@estaca.eu>
2020-10-12 11:54:44 +0200

    Updated Simulink to boost only at mid orbital period

--- simulinkLauncher.m
+++ simulinkLauncher.m
@@ -41,7 +41,7 @@ vernalDirLineX = transpose(linspace(0,1.5*rEarth,100));
 vernalDirLine = [vernalDirLineX zeros(100,1) zeros(100,1)];
 plot3(vernalDirLine(:,1)/1000,vernalDirLine(:,2)/1000,vernalDirLine(:,3)/1000,'LineWidth',1,'DisplayName','Vernal Direction')
 plot3(0,0,0,'og','MarkerFaceColor','b','DisplayName','Origin');
-plot3(chaserFiringPos(:,1)/1000,chaserFiringPos(:,2)/1000,chaserFiringPos(:,3)/1000,'og','MarkerFaceColor','g','DisplayName','Chaser Firing');
+%plot3(chaserFiringPos(:,1)/1000,chaserFiringPos(:,2)/1000,chaserFiringPos(:,3)/1000,'og','MarkerFaceColor','g','DisplayName','Chaser Firing');
 plot3(chaserPos(:,1)/1000,chaserPos(:,2)/1000,chaserPos(:,3)/1000,'LineWidth',1,'LineStyle','-','Marker','none','DisplayName','Chaser Position');
 plot3(targetPos(:,1)/1000,targetPos(:,2)/1000,targetPos(:,3)/1000,'LineWidth',1,'LineStyle','-','Marker','none','DisplayName','Target Position');
 legend;
